284efcbbfdb52319428661ce02d4727bd9ddc76c
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2024-01-21 08:21:54 +0530

    Implement JwtService

---
+++ src/main/java/com/sashmitha/springsecuritydemo/config/JwtService.java
@@ -0,0 +1,37 @@
+package com.sashmitha.springsecuritydemo.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY="RlIyZUVZMHFieU9ZNm5PRVluMVVsUEtNM0lIdy9ra2JJdDFiSDBUSitHQnFOTE1DNXpnMWJnN3NqN0hPT3RHLwo=";
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey(){
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
